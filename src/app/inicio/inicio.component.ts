e9b1f474a412a39c3a54a3b5dfa0173d27572218
Jesus Berisa Nuñez <33457979+llius123@users.noreply.github.com>
2020-02-02 22:27:30 +0100

    Mejoras css markdown

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,27 +1,38 @@
 import { Component, OnInit, HostListener } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import test from "raw-loader!../../assets/test.md" // substitute this path with your README.md file path
-import { MarkdownService } from 'ngx-markdown';
-import { of } from 'rxjs';
-import { delay, first, tap } from 'rxjs/operators';
+// import blockquotes from 'raw-loader!../../assets/markdown/blockquotes.md'
+
 
 @Component({
   selector: 'inicio',
   templateUrl: './inicio.component.html',
   styleUrls: ['./inicio.component.scss']
 })
-export class InicioComponent {
+export class InicioComponent implements OnInit {
   //#region markdown
-  blockquotes = require('raw-loader!../../assets/markdown/blockquotes.md');
-  codeAndSynthaxHighlighting = require('raw-loader!../../assets/markdown/code-and-synthax-highlighting.md');
-  emphasis = require('raw-loader!../../assets/markdown/emphasis.md');
-  headers = require('raw-loader!../../assets/markdown/headers.md');
-  horizontalRule = require('raw-loader!../../assets/markdown/horizontal-rule.md');
-  images = require('raw-loader!../../assets/markdown/images.md');
-  links = require('raw-loader!../../assets/markdown/links.md');
-  lists = require('raw-loader!../../assets/markdown/lists.md');
-  listsDot = require('raw-loader!../../assets/markdown/lists-dot.md');
-  tables = require('raw-loader!../../assets/markdown/tables.md');
+  blockquotes;
+  codeAndSynthaxHighlighting;
+  emphasis;
+  headers;
+  horizontalRule;
+  images;
+  links;
+  lists;
+  listsDot;
+  tables;
+
+  ngOnInit(): void {
+	this.blockquotes = require('raw-loader!../../assets/markdown/blockquotes.md').default;
+	this.codeAndSynthaxHighlighting = require('raw-loader!../../assets/markdown/code-and-synthax-highlighting.md').default;
+	this.emphasis = require('raw-loader!../../assets/markdown/emphasis.md').default;
+	this.headers = require('raw-loader!../../assets/markdown/headers.md').default;
+	this.horizontalRule = require('raw-loader!../../assets/markdown/horizontal-rule.md').default;
+	this.images = require('raw-loader!../../assets/markdown/images.md').default;
+	this.links = require('raw-loader!../../assets/markdown/links.md').default;
+	this.lists = require('raw-loader!../../assets/markdown/lists.md').default;
+	this.listsDot = require('raw-loader!../../assets/markdown/lists-dot.md').default;
+	this.tables = require('raw-loader!../../assets/markdown/tables.md').default;
+	console.log(this.blockquotes)
+  }
 
 
 }
