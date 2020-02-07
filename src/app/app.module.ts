b081125df81add6a8e420ffe56a192d5aca70c21
Jesus Berisa Nuñez <33457979+llius123@users.noreply.github.com>
2020-02-07 20:12:49 +0100

    Pdf viewer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,42 +1,43 @@
+//@Angular
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+//Rutas
 import { AppRoutingModule } from './app-routing.module';
+//Componentes
 import { AppComponent } from './app.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { ColumnaIzquierdaComponent } from './navegacion-izquierda/navegacion-izquierda.component';
-
-import { HttpClientModule, HttpClient } from '@angular/common/http';
-
-import { MarkdownModule, MarkedOptions, MarkedRenderer } from 'ngx-markdown';
 import { MarkDownlistaComponent } from './inicio/markdown-lista/markdown-lista.component';
 import { MarkdownService } from './inicio/markdown.service';
 import { CvComponent } from './cv/cv.component';
 import { ProyectosComponent } from './proyectos/proyectos.component';
 import { ContactoComponent } from './contacto/contacto.component';
 import { MarkdownSimpleComponent } from './inicio/markdown-simple/markdown-simple.component';
+//Librerias externas
+import { MarkdownModule, MarkedOptions, MarkedRenderer } from 'ngx-markdown';
+import { PdfViewerModule } from 'ng2-pdf-viewer';
 
+//Configuracion del markdown
 export function markedOptions(): MarkedOptions {
 	const renderer = new MarkedRenderer();
-  
 	renderer.blockquote = (text: string) => {
-	  return '<blockquote class="blockquote"><p>' + text + '</p></blockquote>';
+		return '<blockquote class="blockquote"><p>' + text + '</p></blockquote>';
 	};
-  
 	return {
-	  renderer: renderer,
-	  gfm: true,
-	  tables: true,
-	  breaks: false,
-	  pedantic: false,
-	  sanitize: false,
-	  smartLists: true,
-	  smartypants: false,
+		renderer: renderer,
+		gfm: true,
+		tables: true,
+		breaks: false,
+		pedantic: false,
+		sanitize: false,
+		smartLists: true,
+		smartypants: false,
 	};
-  }
+}
 
 @NgModule({
-  declarations: [
+declarations: [
 	AppComponent,
 	InicioComponent,
 	ColumnaIzquierdaComponent,
@@ -45,19 +46,20 @@ export function markedOptions(): MarkedOptions {
 	ProyectosComponent,
 	ContactoComponent,
 	MarkdownSimpleComponent
-  ],
-  imports: [
-    BrowserModule,
+],
+imports: [
+	BrowserModule,
 	AppRoutingModule,
 	HttpClientModule,
 	MarkdownModule.forRoot({ 
 		loader: HttpClient,
 		markedOptions: {
-		  provide: MarkedOptions,
-		  useFactory: markedOptions,
-		}, })
-  ],
-  providers: [MarkdownService],
-  bootstrap: [AppComponent]
+			provide: MarkedOptions,
+			useFactory: markedOptions,
+		}, }),
+	PdfViewerModule
+],
+providers: [MarkdownService],
+bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/app/cv/cv.component.ts
+++ src/app/cv/cv.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
 	selector: 'cv',
 	templateUrl: './cv.component.html',
 	styleUrls: ['./cv.component.scss']
 })
-export class CvComponent {
+export class CvComponent implements OnInit {
 
+	public pdfSrc: string;
 	constructor(){}
+
+	ngOnInit(): void {
+		//Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+		//Add 'implements OnInit' to the class.
+		this.pdfSrc = 'https://llius123.github.io/blog-personal-angular/cv.pdf';
+	}
 
 }
